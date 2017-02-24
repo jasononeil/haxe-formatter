62a953d4aa1e1ec0ab011e9bbd2c1a8922a442f1
Gama11 <jensfischer95@gmail.com>
2017-02-24 14:00:54 +0100

    Nicer test output
    Now it actually counts tests correctly (as opposed to counting all tests as one), making the output a bit more satisfying to look at.

--- test/haxeFormatter/TestMain.hx
+++ test/haxeFormatter/TestMain.hx
@@ -3,25 +3,13 @@ package haxeFormatter;
 import haxe.Json;
 import haxe.PosInfos;
 import haxe.io.Path;
-import haxe.unit.TestCase;
-import haxe.unit.TestRunner;
+import haxe.unit.TestResult;
+import haxe.unit.TestRunner.print;
+import haxe.unit.TestStatus;
 import haxeFormatter.Configuration;
 import sys.FileSystem;
 import sys.io.File;
 using StringTools;
-
-class TestMain {
-    public function new() {
-        var runner = new TestRunner();
-        runner.add(new FormattingTestCase());
-        var success = runner.run();
-        Sys.exit(if (success) 0 else 1);
-    }
-
-    static function main() {
-        new TestMain();
-    }
-}
 
 typedef TestConfiguration = {
     > Configuration,
@@ -35,43 +23,88 @@ typedef TestConfiguration = {
     var Noop = "noop";
     /** a second test is generated, with a "flipped config" and source and expected swapped **/
     var Invertible = "invertible";
+
+    public function toString() return this;
 }
 
-class FormattingTestCase extends TestCase {
-    function testAll() {
+class TestMain {
+    static function main() {
+        new TestMain();
+    }
+
+    public function new() {
+        var testResult = new TestResult();
         var dir = "test/haxeFormatter/cases";
         for (file in FileSystem.readDirectory(dir)) {
             if (!file.endsWith(".dump"))
                 continue;
-
-            var absPath = Path.join([Sys.getCwd(), dir, file]);
-            var content = File.getContent(absPath);
-            var nl = "(\r?\n)";
-            var reg = new EReg('$nl$nl---$nl$nl', "g");
-            var segments = reg.split(content);
-            var config:TestConfiguration = try Json.parse(segments[0]) catch(e:Any) {
-                fail('Could not parse config: ${segments[0]}\nReason: $e');
-                null;
-            }
-
-            var isNoopTest = config.testType == Noop;
-            var requiredSegments = if (isNoopTest) 2 else 3;
-            if (segments.length != requiredSegments)
-                fail('Exactly $requiredSegments segments expected, but found ${segments.length}.');
-
-            var name = Path.withoutExtension(file);
-            var source = segments[1];
-            var expected = if (isNoopTest) source else segments[2];
-            assertFormat(name, config, source, expected);
-
-            if (config.testType == Invertible) {
-                // run a second, inverted test
-                assertFormat("Inverted_" + name, invertConfig(config), expected, source);
-            }
+            for (result in processTestDefinition(dir, file))
+                testResult.add(result);
         }
+
+        Sys.println(testResult.toString() + "\n ");
+        Sys.exit(if (testResult.success) 0 else 1);
     }
 
-    function invertConfig(config:Configuration):Configuration {
+    public function processTestDefinition(dir:String, file:String):Array<TestStatus> {
+        var absPath = Path.join([Sys.getCwd(), dir, file]);
+        var content = File.getContent(absPath);
+        var nl = "(\r?\n)";
+        var reg = new EReg('$nl$nl---$nl$nl', "g");
+        var segments = reg.split(content);
+        var config:TestConfiguration = try Json.parse(segments[0]) catch(e:Any) {
+            throw 'Could not parse config: ${segments[0]}\nReason: $e';
+            null;
+        }
+
+        var isNoopTest = config.testType == Noop;
+        var requiredSegments = if (isNoopTest) 2 else 3;
+        if (segments.length != requiredSegments)
+            throw 'Exactly $requiredSegments segments expected, but found ${segments.length}.';
+
+        var name = Path.withoutExtension(file);
+        var source = segments[1];
+        var expected = if (isNoopTest) source else segments[2];
+
+        print('Test: $name ');
+
+        var results = [];
+        results.push(runTest(name, config, source, expected));
+
+        // run a second, inverted test?
+        if (config.testType == Invertible)
+            results.push(runTest("Inverted_" + name, invertConfig(config), expected, source));
+
+        Sys.println("");
+        return results;
+    }
+
+    function runTest(name:String, config:TestConfiguration, sourceCode:String, formattedCode:String, ?c:PosInfos):TestStatus {
+        var status = new TestStatus();
+        status.done = true;
+        status.classname = name;
+        status.method = config.testType.toString();
+
+        switch (Formatter.formatSource(sourceCode, config)) {
+            case Success(result):
+                if (result != formattedCode) {
+                    status.error = 'Test case "$name" failed. Expected:\n\n$formattedCode \n\nbut was:\n\n$result';
+                    status.success = false;
+                    print("E");
+                } else {
+                    status.success = true;
+                    print(".");
+                }
+            case Failure(reason):
+                status.error = 'Formatting failed with $reason';
+                status.success = false;
+                print("W");
+        }
+
+        return status;
+    }
+
+    function invertConfig(config:TestConfiguration):TestConfiguration {
         config = Reflect.copy(config);
         // there has to be a smarter way
         if (config.imports != null && config.imports.sort != null) {
@@ -89,24 +122,5 @@ class FormattingTestCase extends TestCase {
             colon.after = flipWhitespacePolicy(colon.after);
         }
         return config;
-    }
-
-    function fail(msg:String, ?c:PosInfos):Void {
-        currentTest.done = true;
-        currentTest.success = false;
-        currentTest.error = msg;
-        currentTest.posInfos = c;
-        throw currentTest;
-    }
-
-    function assertFormat(name:String, config:Configuration, sourceCode:String, formattedCode:String, ?c:PosInfos) {
-        switch (Formatter.formatSource(sourceCode, config)) {
-            case Success(result):
-                if (result != formattedCode)
-                   fail('Test case "$name" failed. Expected:\n\n$formattedCode \n\nbut was:\n\n$result');
-            case Failure(reason):
-                fail('Formatting failed with $reason');
-        }
-        assertTrue(true);
     }
 }
