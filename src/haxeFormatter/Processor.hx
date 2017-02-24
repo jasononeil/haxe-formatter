c64144ae682c86d684a77f1996a79a086a23c702
Gama11 <jensfischer95@gmail.com>
2017-02-24 22:06:00 +0100

    Adapt to hxparser changes

--- src/haxeFormatter/Processor.hx
+++ src/haxeFormatter/Processor.hx
@@ -6,7 +6,6 @@ import hxParser.Tree;
 
 class Processor {
     var config:Config;
-    var numDecls:Int;
     var curNode:String;
     var prevNode:String;
     var parentNodes:Array<String>;
@@ -18,7 +17,6 @@ class Processor {
 
     public function process(tree:Tree, parentNodes:Array<String>):Tree {
         this.parentNodes = parentNodes;
-        numDecls = 0;
         return {
             kind: processTreeKind(tree.kind),
             start: tree.start,
@@ -43,9 +41,8 @@ class Processor {
     function processNode(name:String, children:Array<Tree>):TreeKind {
         switch (name) {
             case "decls":
-                if (numDecls == 0 && config.imports.sort)
+                if (config.imports.sort)
                     children = sortImports(children);
-                numDecls++;
             case _:
         }
         return Node(name, children.map(process.bind(_, parentNodes.concat([name]))));
@@ -114,25 +111,37 @@ class Processor {
         }
     }
 
-    function sortImports(importDecls:Array<Tree>):Array<Tree> {
-        if (importDecls.length == 0)
-            return importDecls;
+    function sortImports(decls:Array<Tree>):Array<Tree> {
+        var firstImport = getFirstImportDecl(decls);
+        if (firstImport == null)
+            return decls;
 
-        var firstTrivia = getImportTrivia(importDecls[0].kind);
+        var firstTrivia = getImportTrivia(firstImport);
         var leadingTrivia = firstTrivia.leading;
         firstTrivia.leading = null;
 
         // TODO: consider Token("using")
-        importDecls.sort(function(tree1, tree2) return switch[tree1.kind, tree2.kind] {
-            case [Node("decl", [{kind: Token("import",_)},name1,_]), Node("decl", [{kind:Token("import",_)},name2,_])]:
+        decls.sort(function(tree1, tree2) return switch[tree1.kind, tree2.kind] {
+            case [Node("import_decl", [{kind: Token("import",_)},name1,_]), Node("import_decl", [{kind:Token("import",_)},name2,_])]:
                 Reflect.compare(print(name1), print(name2));
-            case [Node("decl", [{kind: Token("import",_)},_,_]),_]: -1;
-            case [_,Node("decl", [{kind: Token("import",_)},_,_])]: 1;
+            case [Node("import_decl", [{kind: Token("import",_)},_,_]),_]: -1;
+            case [_,Node("import_decl", [{kind: Token("import",_)},_,_])]: 1;
             case _: 0;
         });
 
-        getImportTrivia(importDecls[0].kind).leading = leadingTrivia;
-        return importDecls;
+        getImportTrivia(decls[0].kind).leading = leadingTrivia;
+        return decls;
+    }
+
+    function getFirstImportDecl(decls:Array<Tree>):TreeKind {
+        for (decl in decls) {
+            switch (decl.kind) {
+                case Node(name,children) if (name == "import_decl" || name == "using_decl"):
+                    return decl.kind;
+                case _:
+            }
+        }
+        return null;
     }
 
     function getImportTrivia(treeKind:TreeKind):Trivia {
@@ -140,13 +149,13 @@ class Processor {
             case Node(name, children) if (children.length > 0): switch (children[0].kind) {
                 case Token(token, trivia) if (token == "import" || token == "using"):
                     trivia;
-                case _: null;
+                case _: unexpected("");
             }
-            case _: null;
+            case _: unexpected("");
         }
     }
 
     inline function unexpected(what:String) {
-        throw 'Unexpected $what';
+        return throw 'Unexpected $what';
     }
 }
